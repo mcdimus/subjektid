b3bcf7cd6cacded8ccf1b9f4391d8494d914731f
123Noob312@gmail.com <123Noob312@gmail.com@229ed73d-0101-cd84-4742-b6bca5d57c58>
2012-05-13 20:27:18 +0000

    SubjectsDAO.auth() for user authorization.

--- subjektid/src/backend/DA/SubjectsDAO.java
+++ subjektid/src/backend/DA/SubjectsDAO.java
@@ -1,18 +1,22 @@
 package backend.DA;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
-import backend.model.Address;
+import log.MyLogger;
+
+import backend.model.Employee;
+import backend.model.UserAccount;
 
 public class SubjectsDAO {
 	
-	String url, user, password;
+	private String url, user, password;
 	
 	public SubjectsDAO() {
 		try {
@@ -22,51 +26,86 @@ public class SubjectsDAO {
 			password = bundle.getString("password");
 			Class.forName(bundle.getString("Driver"));
 		} catch(Exception e) {
+			MyLogger.log("SubjectsDAO(): ", e.getMessage());
 			e.printStackTrace();
 		}
 	}
-
-	public static void main(String[] args) {
-		SubjectsDAO dao = new SubjectsDAO();
-		dao.findAddresses();
-	}
 	
-	public void findAddresses() {
-		List<Address> addresses = null;
+	public Employee auth(UserAccount ua) {
+		ua.setPassw(generateMD5(ua.getUsername() + ua.getPassw()));
 		Connection db;
 		ResultSet results;
+		Employee employee = null;
 		try {
 			db = DriverManager.getConnection(url, user, password);
 			Statement st = db.createStatement();
-			addresses = new ArrayList<Address>();
-			results = st.executeQuery("SELECT * FROM address");
-			while(results.next()) {
-				Address address = new Address();
-				address.setAddress(results.getLong("address"));
-				address.setAddressTypeFk(results.getLong("address_type_fk"));
-				address.setSubjectFk(results.getLong("subject_fk"));
-				address.setSubjectTypeFk(results.getLong("subject_type_fk"));
-				address.setCountry(results.getString("country"));
-				address.setCounty(results.getString("county"));
-				address.setTownVillage(results.getString("town_village"));
-				address.setStreetAddress(results.getString("street_address"));
-				address.setZipcode(results.getString("zipcode"));
-				addresses.add(address);
-			}
+			results = st.executeQuery("SELECT e.employee, e.person_fk,"
+					+ "e.enterprise_fk, e.struct_unit_fk, e.active FROM "
+					+ "employee E INNER JOIN user_account UA ON E.employee"
+					+ "= UA.subject_fk WHERE UA.username='" + ua.getUsername()
+					+ "' AND UA.passw='"+ ua.getPassw() + "'");
+			results.next();
+			employee = new Employee(results.getLong("employee"),
+					results.getLong("person_fk"),
+					results.getLong("enterprise_fk"),
+					results.getLong("struct_unit_fk"),
+					results.getString("active"));
 			db.close();
 		} catch(Exception e) {
+			MyLogger.log("SubjectsDAO.auth(): ", e.getMessage());
 			e.printStackTrace();
-		} finally {
-			for (int i = 0; i < addresses.size(); i++) {
-				Address address = addresses.get(i);
-				System.out.printf("%d\t%d\t%d\t%d\t%s\t%s\t%s\t%s\t%s\r\n", 
-						address.getAddress(), address.getAddressTypeFk(),
-						address.getSubjectFk(), address.getSubjectTypeFk(),
-						address.getCountry(), address.getCounty(),
-						address.getTownVillage(), address.getStreetAddress(),
-						address.getZipcode());
-			}
 		}
-
+		return employee;
 	}
+	
+	static String generateMD5(String passw) {
+		try {
+			MessageDigest m = MessageDigest.getInstance("MD5");
+			BigInteger bi = new BigInteger(1, m.digest(passw.getBytes()));
+			return String.format("%1$032X", bi);
+		} catch (NoSuchAlgorithmException e) {
+			MyLogger.log("SubjectsDAO.generateMD5(): ", e.getMessage());
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+//	public void findAddresses() {
+//		List<Address> addresses = null;
+//		Connection db;
+//		ResultSet results;
+//		try {
+//			db = DriverManager.getConnection(url, user, password);
+//			Statement st = db.createStatement();
+//			addresses = new ArrayList<Address>();
+//			results = st.executeQuery("SELECT * FROM address");
+//			while(results.next()) {
+//				Address address = new Address();
+//				address.setAddress(results.getLong("address"));
+//				address.setAddressTypeFk(results.getLong("address_type_fk"));
+//				address.setSubjectFk(results.getLong("subject_fk"));
+//				address.setSubjectTypeFk(results.getLong("subject_type_fk"));
+//				address.setCountry(results.getString("country"));
+//				address.setCounty(results.getString("county"));
+//				address.setTownVillage(results.getString("town_village"));
+//				address.setStreetAddress(results.getString("street_address"));
+//				address.setZipcode(results.getString("zipcode"));
+//				addresses.add(address);
+//			}
+//			db.close();
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//		} finally {
+//			for (int i = 0; i < addresses.size(); i++) {
+//				Address address = addresses.get(i);
+//				System.out.printf("%d\t%d\t%d\t%d\t%s\t%s\t%s\t%s\t%s\r\n", 
+//						address.getAddress(), address.getAddressTypeFk(),
+//						address.getSubjectFk(), address.getSubjectTypeFk(),
+//						address.getCountry(), address.getCounty(),
+//						address.getTownVillage(), address.getStreetAddress(),
+//						address.getZipcode());
+//			}
+//		}
+//	}
+	
 }
