2fde3194a9409d8c69daf56ad0d172f874a54650
123Noob312@gmail.com <123Noob312@gmail.com@229ed73d-0101-cd84-4742-b6bca5d57c58>
2012-05-15 01:06:05 +0000

    Fixed a bit hibernate xml mappings, added some style to the style.css, SubjectsORM.search() in progress.

--- trunk/subjektid/src/backend/DA/SubjectsORM.java
+++ trunk/subjektid/src/backend/DA/SubjectsORM.java
@@ -1,10 +1,17 @@
 package backend.DA;
 
+import general.Utils;
+
+import java.util.HashMap;
 import java.util.List;
+
+import log.MyLogger;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+
+import backend.model.UserAccount;
 
 public class SubjectsORM {
 	
@@ -18,6 +25,7 @@ public class SubjectsORM {
 		    data = (List<T>) session.createQuery("from "
 		    		+ _class.getName()).list();
 		} catch(Exception e) {
+			MyLogger.log("SubjectsORM.findAll(): ", e.getMessage());
 			e.printStackTrace();
 		}
 		session.close();
@@ -36,6 +44,7 @@ public class SubjectsORM {
 			q.setInteger("subj_id", id);		
 		    data = (List<T>) q.list();
 		} catch(Exception e) {
+			MyLogger.log("SubjectsORM.findById(): ", e.getMessage());
 			e.printStackTrace();
 		}
 		session.close();
@@ -52,11 +61,39 @@ public class SubjectsORM {
 			tx.commit();
 			return true;
 		 } catch(Exception e) {
+			MyLogger.log("SubjectsORM.saveOrUpdate(): ", e.getMessage());
 			 e.printStackTrace();
 		      if (tx != null) {
 		    	  tx.rollback();
 		      }
 		      return false;
 		 } 
+	}
+	
+	public boolean saveUserAcc(UserAccount user) {
+		user.setPassw(Utils.generateMD5(user.getUsername()
+				+ user.getPassw()));
+		return saveOrUpdate(user);
+	}
+
+	@SuppressWarnings("unchecked")
+	public <T> List<T> search(HashMap<String, String> fields, Class<T> _class) {
+		Session session = null;
+		List<T> data = null;
+		try {
+			session = HibernateUtil.getSessionFactory().getCurrentSession();
+			session.beginTransaction();
+			String queryStr = "from ";
+			for (String key : fields.keySet()) {
+				// TODO: redo! number IN(int, int) is not possible to
+				// connect with this
+			}
+		    data = (List<T>) session.createQuery(queryStr).list();
+		} catch(Exception e) {
+			MyLogger.log("SubjectsORM.findById(): ", e.getMessage());
+			e.printStackTrace();
+		}
+		session.close();
+		return data;
 	}
 }
